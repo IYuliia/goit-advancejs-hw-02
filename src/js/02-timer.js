05666722c502b7edc4a7973b3026d042f53c0b6c
IYuliia <catnew2012@gmail.com>
2023-11-29 09:41:10 +0100

    removed reset time from timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -19,13 +19,10 @@ const hoursEl = document.querySelector('[data-hours]');
 const minsEl = document.querySelector('[data-minutes]');
 const secsEl = document.querySelector('[data-seconds]');
 
-const initialTimerValue = timer.textContent;
 startBtn.disabled = true;
 
 let timerInterval = null;
-let flatpickrInstance;
-
-flatpickrInstance = flatpickr('input#datetime-picker', options);
+let flatpickrInstance = flatpickr('input#datetime-picker', options);
 
 startBtn.addEventListener('click', () => {
   const selectedDate = flatpickrInstance.selectedDates[0];
@@ -54,25 +51,18 @@ function handleInvalidDate() {
 
 function startTimer(selectedDate) {
   startBtn.disabled = true;
+  document.querySelector('input#datetime-picker').disabled = true;
   clearInterval(timerInterval);
   timerInterval = setInterval(() => {
     const timeLeft = selectedDate.getTime() - Date.now();
     if (timeLeft <= 0) {
       clearInterval(timerInterval);
-      resetTimer();
-      startBtn.disabled = true;
+      document.querySelector('input#datetime-picker').disabled = false;
     } else {
       const { days, hours, minutes, seconds } = convertMs(timeLeft);
       updateTimer(days, hours, minutes, seconds);
-      document.querySelector('input#datetime-picker').disabled = true;
     }
   }, 1000);
-}
-
-function resetTimer() {
-  timer.textContent = initialTimerValue;
-  startBtn.disabled = false;
-  document.querySelector('input#datetime-picker').disabled = false;
 }
 
 function updateTimer(days, hours, minutes, seconds) {
@@ -83,27 +73,18 @@ function updateTimer(days, hours, minutes, seconds) {
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
 function formatTime(number) {
   return number.toString().padStart(2, '0');
